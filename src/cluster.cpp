a49c242ee64a63996fd55b88e65f599db5b131be
rkarnik <rkarnik>
2007-08-21 19:45:23 +0000

    - use arrays instead of vectors - make kmeans clustering actually work

--- src/cluster.cpp
+++ src/cluster.cpp
@@ -1,18 +1,52 @@
 #include "cluster.h"
 
-Cluster::Cluster (const vector<vector<float> >& exprtab, const vector<string>& names):
-expr(exprtab),
-ngenes(0),
-membership(exprtab.size(), 0),
-mean(exprtab[0].size()),
-nameset(names),
-dirty(false) {
+Cluster::~Cluster() {
+	delete [] membership;
+	delete [] mean;
+}
+
+Cluster::Cluster(const Cluster& c) {
+	init(c.expr, c.npoints, c.nameset);
+	nmembers = c.nmembers;
+	for(int i = 0; i < nameset.size(); i++) {
+		membership[i] = c.membership[i];
+	}
+	dirty = true;
+}
+
+const Cluster& Cluster::operator= (const Cluster& c) {
+	delete [] membership;
+	delete [] mean;
+	init(c.expr, c.npoints, c.nameset);
+	nmembers = c.nmembers;
+	for(int i = 0; i < nameset.size(); i++) {
+		membership[i] = c.membership[i];
+	}
+	dirty = true;
+	return *this;
+}
+
+void Cluster::init (float** exprtab, int numexpr, const vector<string>& names) {
+	expr = exprtab;
+	nmembers = 0;
+	ngenes = names.size();
+	npoints = numexpr;
+	membership = new int[ngenes];
+	for(int g = 0; g < ngenes; g++) {
+		membership[g] = 0;
+	}
+	mean = new float[npoints];
+	for(int p = 0; p < npoints; p++) {
+		mean[p] = 0;
+	}
+	nameset = names;
+	dirty = false;
 }
 
 void Cluster::add_gene(const int gene) {
 	if (membership[gene] == 0) {
 		membership[gene] = 1;
-		ngenes++;
+		nmembers++;
 	}
 	dirty = true;
 }
@@ -20,60 +54,62 @@ void Cluster::add_gene(const int gene) {
 void Cluster::remove_gene(const int gene) {
 	if (membership[gene] == 1) {
 		membership[gene] = 0;
-		ngenes--;
+		nmembers--;
 	}
 	dirty = true;
 }
 
-void Cluster::set_mean(const vector<float>& new_mean) {
-	mean.assign(new_mean.begin(), new_mean.end());
+bool Cluster::is_member(const int gene) {
+	return (membership[gene] == 1);
 }
 
-vector<float>& Cluster::get_mean() {
+float* Cluster::get_mean() {
+	if (dirty) calc_mean();
 	return mean;
 }
 
 int Cluster::size() const {
-	return ngenes;
+	return nmembers;
 }
 
 void Cluster::calc_mean() {
 	if (dirty) {
 		int i, j;
-		if (expr.size() > 1) {
-			for (i = 0; i < expr[0].size(); i++) {
+		if (npoints > 1) {
+			for (i = 0; i < npoints; i++) {
 				mean[i] = 0;
-				for (j = 0; j < expr.size(); j++) {
+				for (j = 0; j < nameset.size(); j++) {
 					mean[i] += membership[j] * expr[j][i];
 				}
-				mean[i] /= ngenes;
+				mean[i] /= nmembers;
 			}
 		}
 		dirty = false;
 	}
 }
 
-float Cluster::corr (const vector<float>& pattern) const {
+float Cluster::corr (const float* pattern) const {
 	int i;
 	float u1, u2;
 	float s1, s2;
 	float c;
-	for (i = 0; i < mean.size(); i++) {
+	u1 = u2 = s1 = s2 = c = 0.0;
+	for (i = 0; i < npoints; i++) {
 		u1 += mean[i];
 		u2 += pattern[i];
 		s1 += mean[i] * mean[i];
 		s2 += pattern[i] * pattern[i];
 	}
-	u1 /= mean.size();
-	u2 /= pattern.size();
-	for (i = 0; i < mean.size(); i++) {
+	u1 /= npoints;
+	u2 /= npoints;
+	for (i = 0; i < npoints; i++) {
 		c += (mean[i] - u1) * (pattern[i] - u2);
 	}
-	s1 = s1/mean.size() - u1 * u1;
-	s2 = s2/pattern.size() - u2 * u2;
+	s1 = s1/npoints - u1 * u1;
+	s2 = s2/npoints - u2 * u2;
 	if (s1 > 0 && s2 > 0) {
 		c /= sqrt(s1 * s2);
-		c /= mean.size();
+		c /= npoints;
 	} else {
 		c = 0;
 	}
@@ -81,7 +117,7 @@ float Cluster::corr (const vector<float>& pattern) const {
 }
 
 void Cluster::genes(vector<int>& genes) const {
-	for (int g = 0; g < membership.size(); g++) {
+	for (int g = 0; g < nameset.size(); g++) {
 		if (membership[g] == 1) {
 			genes.push_back(g);
 		}
--- src/cluster.h
+++ src/cluster.h
@@ -9,23 +9,29 @@
 
 
 class Cluster {
+	float** expr;
 	vector<string> nameset;
-	vector<vector<float> > expr;
+	int nmembers;
 	int ngenes;
-	vector<int> membership;
-	vector<float> mean;
+	int npoints;
+	int* membership;
+	float* mean;
 	bool dirty;
 	
 public:
-	Cluster (const vector<vector<float> >& exprtab, const vector<string>& names);
+	Cluster() {};
+	Cluster(const Cluster& c);
+	~Cluster();
+	const Cluster& operator= (const Cluster& c);
+	void init (float** exprtab, int numexpr, const vector<string>& names);
 	void add_gene (const int gene);                         // Add gene to this cluster
 	void remove_gene (const int gene);                      // Remove gene from this cluster
+	bool is_member(const int gene);                         // Return whether the gene is a member of this cluster
 	int size() const;                                       // Return size of this cluster
 	void calc_mean();                                       // Calculate the mean for this cluster
-	void set_mean (const vector<float>& new_mean);          // Set the new mean for this cluster
-	vector<float>& get_mean();                              // Get the mean for this cluster
-	float corr (const vector<float>& pattern) const;        // Calculate the correlation between the cluster mean and 'pattern'
-	void genes(vector<int>& genes) const;                   // Return the genes that are assigned to this cluster             
+	float* get_mean();                                      // Get the mean for this cluster
+	float corr (const float* pattern) const;                // Calculate the correlation between the cluster mean and 'pattern'
+	void genes(vector<int>& genes) const;                   // Return the genes that are assigned to this cluster
 };
 
 #endif
