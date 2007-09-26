e9a64e1f99d89fee3c62d85aa6525521cfd72043
rkarnik <rkarnik>
2007-09-26 20:00:56 +0000

    Remove old main() files

--- src/main-AlignACE.cpp
+++
@@ -1,8 +0,0 @@
-//Copyright 1999 President and Fellows of Harvard University
-//main.cpp
-
-#include "alignace.h"
-
-int main(int argc, char *argv[]) {
-  cAlignACE(argc, argv);
-}
--- src/main-CompareACE.cpp
+++
@@ -1,8 +0,0 @@
-//Copyright 1999 President and Fellows of Harvard University
-//main-CompareACE.cpp
-
-#include "compareace.h"
-
-int main(int argc, char *argv[]) {
-  cCompareACE(argc, argv);
-}
