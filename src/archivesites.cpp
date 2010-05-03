93a21dc15750c1ac91c41033b6963724299c13dc
rkarnik <rkarnik>
2010-05-03 20:50:06 +0000

    Use new method to compare motifs

--- src/archivesites.cpp
+++ src/archivesites.cpp
@@ -3,87 +3,84 @@
 
 #include "archivesites.h"
 
-ArchiveSites::ArchiveSites(Seqset& seq, double sim_cut) : 
+ArchiveSites::ArchiveSites(Seqset& seq, double sim_cut, double* pseudo) : 
 arch_seqset(seq),
-arch_comp(0),
+archive(0),
 arch_sim_cutoff(sim_cut),
+pseudo(pseudo),
 arch_min_visits(3) {
 }
 
 bool ArchiveSites::check_motif(const Motif& m) {
-  double cmp;
-	CompareACESites c(m);
-	vector<CompareACESites>::iterator iter = arch_comp.begin();
-  for(; iter != arch_comp.end() && m.get_spec() <= iter->motif()->get_spec(); ++iter){
-    cmp = c.compare(*iter);
-		if(cmp > arch_sim_cutoff && iter->motif()->get_dejavu() >= arch_min_visits)
+	float cmp;
+	vector<Motif>::iterator iter = archive.begin();
+  for(; iter != archive.end() && m.get_spec() <= iter->get_spec(); ++iter){
+    cmp = iter->compare(m);
+		if(cmp > arch_sim_cutoff && iter->get_dejavu() >= arch_min_visits)
 			return false;
   }
   return true;
 }
 
 bool ArchiveSites::consider_motif(const Motif& m) {
-  if(m.get_spec() < 1) return false; 
-  
-	CompareACESites c(m);
-  double cmp;
+  if(m.get_spec() < 1) return false;
+	float cmp;
 
 	// Check if similar to better motif.
 	// If so, increment dejavu for better motif and return false
-	vector<CompareACESites>::iterator iter = arch_comp.begin();
-	for(; iter != arch_comp.end() && m.get_spec() <= iter->motif()->get_spec(); ++iter) {
-		cmp = c.compare(*iter);
+	vector<Motif>::iterator iter = archive.begin();
+	for(; iter != archive.end() && m.get_spec() <= iter->get_spec(); ++iter) {
+		cmp = iter->compare(m);
 		if(cmp > arch_sim_cutoff) {
-			iter->motif()->inc_dejavu();
+			iter->inc_dejavu();
 			return false;
 		}
 	}
 	
 	// There are no better motifs similar to this one, so we add
 	// Step 1: Delete similar motifs with lower scores
-	while(iter != arch_comp.end()) {
-		assert(m.get_spec() > iter->motif()->get_spec());
-		cmp = c.compare(*iter);
+	while(iter != archive.end()) {
+		assert(m.get_spec() > iter->get_spec());
+		cmp = iter->compare(m);
 		if(cmp > arch_sim_cutoff)
-			iter = arch_comp.erase(iter);
+			iter = archive.erase(iter);
 		else
 			++iter;
 	}
 	
 	// Step 2: Add the new motif at the correct position by score
-	for(iter = arch_comp.begin(); iter != arch_comp.end(); ++iter)
-		if(iter->motif()->get_spec() < m.get_spec()) break;
-	arch_comp.insert(iter, c);
+	for(iter = archive.begin(); iter != archive.end(); ++iter)
+		if(iter->get_spec() < m.get_spec()) break;
+	archive.insert(iter, m);
 	return true;
 }
 
 Motif* ArchiveSites::return_best(const int i) {
-  return arch_comp[i].motif();
+  return &archive[i];
 }
 
 void ArchiveSites::clear() {
-	vector<CompareACESites>().swap(arch_comp);
+	vector<Motif>().swap(archive);
 }
 
 void ArchiveSites::read(istream& archin) {
 	char line[200];
 	while(archin.getline(line, 200)) {
 		if(strstr(line, "Motif")) {
-			Motif m(arch_seqset, 12);
+			Motif m(arch_seqset, 12, pseudo);
 			m.read(archin);
-			CompareACESites c(m);
-			arch_comp.push_back(c);
+			archive.push_back(m);
 		}
 	}
 }
 
 void ArchiveSites::write(ostream& archout) {
 	int i = 0;
-	vector<CompareACESites>::iterator iter = arch_comp.begin();
-	for(; iter != arch_comp.end(); ++iter) {
-		if(iter->motif()->get_spec() > 1) {
+	vector<Motif>::iterator iter = archive.begin();
+	for(; iter != archive.end(); ++iter) {
+		if(iter->get_spec() > 1) {
 			archout << "Motif " << i + 1 << "\n";
-			iter->motif()->write(archout);
+			iter->write(archout);
 			i++;
 		}
 	}
--- src/archivesites.h
+++ src/archivesites.h
@@ -4,17 +4,19 @@
 #ifndef _archivesites
 #define _archivesites
 #include "standard.h"
-#include "compareace.h"
+#include "seqset.h"
+#include "motif.h"
 
 class ArchiveSites{
   const Seqset& arch_seqset;
-	vector<CompareACESites> arch_comp;
+	vector<Motif> archive;
   double arch_sim_cutoff;
+	double* pseudo;
   int arch_min_visits;
 
  public:
-  ArchiveSites(Seqset& seq, double sim_cut);
-	int nmots() const { return arch_comp.size(); }
+  ArchiveSites(Seqset& seq, double sim_cut, double* pseudo);
+	int nmots() const { return archive.size(); }
   bool check_motif(const Motif& m);               // Returns true if no better motif, false otherwise
   bool consider_motif(const Motif& m);            // Returns true if motif was added, false otherwise
   Motif* return_best(const int i=0);
