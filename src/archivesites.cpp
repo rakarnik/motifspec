2fd59524a02880d439b0ae4d77c723c071128641
rkarnik <rkarnik>
2011-05-25 13:18:51 +0000

    Add some debug messages until comparing is ironed out

--- src/archivesites.cpp
+++ src/archivesites.cpp
@@ -32,7 +32,7 @@ bool ArchiveSites::consider_motif(const Motif& m) {
 	vector<Motif>::iterator iter = archive.begin();
 	for(; iter != archive.end() && m.get_motif_score() <= iter->get_motif_score(); ++iter) {
 		cmp = iter->compare(m);
-		// cerr << "Comparing with motif " << motnum << ", score was " << cmp << "\n";
+		cerr << "Comparing with motif " << motnum << ", score was " << cmp << "\n";
 		if(cmp > arch_sim_cutoff) {
 			iter->inc_dejavu();
 			return false;
@@ -48,6 +48,7 @@ bool ArchiveSites::consider_motif(const Motif& m) {
 	while(iter != archive.end()) {
 		assert(m.get_motif_score() > iter->get_motif_score());
 		cmp = iter->compare(m1);
+		cerr << "Comparing with motif " << motnum << ", score was " << cmp << "\n";
 		if(cmp > arch_sim_cutoff) {
 	 		iter = archive.erase(iter);
 			m1.inc_dejavu();
@@ -57,6 +58,7 @@ bool ArchiveSites::consider_motif(const Motif& m) {
 		}
 		motnum++;
 	}
+	cerr << "Deleted " << delcount << " similar motifs\n";
 
 	
 	// Step 2: Add the new motif at the correct position by score
