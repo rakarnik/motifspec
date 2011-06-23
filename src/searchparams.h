97189c5782dd650adb6bb4c2f26ab1aed8a81715
rkarnik <rkarnik>
2011-06-23 03:50:44 +0000

    Re-indent with tabs

--- src/searchparams.h
+++ src/searchparams.h
@@ -2,24 +2,24 @@
 #define _searchparams
 
 struct SearchParams {
-  int expect;						           // number of expected sites
-  double weight;				           // fractional weight on priors
-  double psfact;				           // psfact * numsites=npseudo
-  double npseudo;				           // number of pseudo counts
-  vector<double> backfreq;         // array for gc content
-  vector<double> pseudo;           // pseudocounts for any frequency calculations
-  int maxlen;						           // maximum length of sites
-  int npass;
-  int minpass;
-  int nruns;
+	int expect;						           // number of expected sites
+	double weight;				           // fractional weight on priors
+	double psfact;				           // psfact * numsites=npseudo
+	double npseudo;				           // number of pseudo counts
+	vector<double> backfreq;         // array for gc content
+	vector<double> pseudo;           // pseudocounts for any frequency calculations
+	int maxlen;						           // maximum length of sites
+	int npass;
+	int minpass;
+	int nruns;
 	float minprob[4];                // minimum P(m|s) cutoff in each phase
 	float minscore[4];               // minimum score cutoff in each phase
-  bool fragment;
-  int seed;
-  double select;
-  int flanking;
-  int undersample;
-  int oversample;
+	bool fragment;
+	int seed;
+	double select;
+	int flanking;
+	int undersample;
+	int oversample;
 	int minsize;
 	float mincorr;
 };
