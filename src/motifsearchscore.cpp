3d332a24d931dbc38c6cfa2ab283298058bf984a
Rahul Karnik <rahul@genebrew.com>
2011-09-13 11:54:22 +0530

    Fix indentation in constructor

--- src/motifsearchscore.cpp
+++ src/motifsearchscore.cpp
@@ -9,19 +9,19 @@ MotifSearchScore::MotifSearchScore(const vector<string>& names,
 MotifSearch(names, seqs, nc, order, sim_cut),
 scores(sctab),
 scranks(ngenes) {
-		float sc_max = DBL_MAX;
-		float sc_min = DBL_MIN;
-		vector<float>::iterator sciter = scores.begin();
-		for(; sciter != scores.end(); ++sciter) {
-			sc_max = max(sc_max, *sciter);
-			sc_min = min(sc_min, *sciter);
-		}
-		for(int i = 0; i < ngenes; i++) {
-			scores[i] = (scores[i] - sc_min)/(sc_max - sc_min);
-			scranks[i].id = i;
-			scranks[i].score = scores[i];
-		}
-		sort(scranks.begin(), scranks.end(), isc);
+	float sc_max = DBL_MAX;
+	float sc_min = DBL_MIN;
+	vector<float>::iterator sciter = scores.begin();
+	for(; sciter != scores.end(); ++sciter) {
+		sc_max = max(sc_max, *sciter);
+		sc_min = min(sc_min, *sciter);
+	}
+	for(int i = 0; i < ngenes; i++) {
+		scores[i] = (scores[i] - sc_min)/(sc_max - sc_min);
+		scranks[i].id = i;
+		scranks[i].score = scores[i];
+	}
+	sort(scranks.begin(), scranks.end(), isc);
 	reset_search_space();
 }
 
