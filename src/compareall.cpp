13e3a95e64f914aa4e25a6f54c34e09210782e83
Rahul Karnik <rahul@genebrew.com>
2011-11-16 00:53:13 -0500

    Update to use new ArchiveSites constructor

--- src/compareall.cpp
+++ src/compareall.cpp
@@ -9,10 +9,11 @@ int main(int argc, char** argv) {
 	get_fasta_fast(seqfile.c_str(), seqs);
 	Seqset seqset(seqs);
 	BGModel bgm(seqset, 0);
-	vector<double> pseudo(4);
-	pseudo[0] = pseudo[3] = (1 - bgm.gcgenome())/2.0;
-	pseudo[1] = pseudo[2] = bgm.gcgenome()/2.0;
-	ArchiveSites archive(seqset, bgm, 0.8, pseudo);
+	vector<double> backfreq(4);
+	backfreq[0] = backfreq[3] = (1 - bgm.gcgenome())/2.0;
+	backfreq[1] = backfreq[2] = bgm.gcgenome()/2.0;
+	vector<double> pseudo(backfreq);
+	ArchiveSites archive(seqset, bgm, 0.8, pseudo, backfreq);
 	ifstream acestream(acefile.c_str());
 	archive.read(acestream);
 	acestream.close();
