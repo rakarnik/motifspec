f60ceb5d1ea771efea38f43e4e6c178e370d10c4
Rahul Karnik <rahul@genebrew.com>
2011-12-05 18:28:22 -0500

    Go back to a CompareACE style of motif comparison. Find most informative columns in first motif and align with most correlated columns in second motif.

--- src/motifcompare.cpp
+++ src/motifcompare.cpp
@@ -6,85 +6,61 @@ MotifCompare::MotifCompare(const Seqset& s, const BGModel& bgm)
 }
 
 float MotifCompare::compare(const Motif& m1, const Motif& m2) const {
-	vector<float> scores1, scores2;
-	int c, p, len, window_size;
-	int olap = 2; // overlap required between motifs
-	bool s;
-	float sc, max_sc_1, max_sc_2;
+	int cols = 5;
 	
-	// Set up score matrices
-	double* sm1 = new double[4 * m1.ncols()];
-	m1.calc_score_matrix(sm1);
-	double* sm2 = new double[4 * m2.ncols()];
-	m2.calc_score_matrix(sm2);
+	int fmsize1 = m1.get_width() + 2 * m1.ncols();
+	vector<float> fm1(fmsize1 * 4);
+	m1.freq_matrix_extended(fm1);
 	
-	int w1 = m1.get_width();
-	int w2 = m2.get_width();
+	int fmsize2 = m2.get_width() + 2 * m2.ncols();
+	vector<float> fm2(fmsize2 * 4);
+	m2.freq_matrix_extended(fm2);
 	
-	// Score sites for this motif with both PWMs and background model
-	vector<Site>::const_iterator siter1 = m1.sites().begin();
-	for(; siter1 != m1.sites().end(); ++siter1) {
-		c = siter1->chrom();
-		p = siter1->posit();
-		s = siter1->strand();
-		len = seqset.len_seq(c);
-		
-		max_sc_1 = -1000;
-		window_size = w2 - olap;
-		for(int i = max(0, p - window_size); i < min(len - w1 - 1, p + window_size); i++) {
-			sc = m1.score_site(sm1, c, i, s) - bgmodel.score_site(m1.first_column(), m1.last_column(), w1, c, i, s);
-			sc /= m1.ncols();
-			if(sc > max_sc_1)
-				max_sc_1 = sc;
+	// Find columns with the highest information content
+	vector<struct idscore> csc(fmsize1);
+	float ent;
+	for(int i = 0; i < fmsize1; i++) {
+		ent = 0.0;
+		for(int j = i * 4; j < (i + 1) * 4; j++) {
+			ent += fm1[j] > 0? -fm1[j] * log(fm1[j]) : 0;
 		}
-		scores1.push_back(max_sc_1);
-		
-		max_sc_2 = -1000;
-		window_size = w1 - olap;
-		for(int i = max(0, p - window_size); i < min(len - w2 - 1, p + window_size); i++) {
-			sc = m2.score_site(sm2, c, i, s) - bgmodel.score_site(m2.first_column(), m2.last_column(), w2, c, i, s);
-			sc /= m2.ncols();
-			if(sc > max_sc_2)
-				max_sc_2 = sc;
-		}
-		scores2.push_back(max_sc_2);
+		csc[i].id = i;
+		csc[i].score = ent;
+	}
+	sort(csc.begin(), csc.end(), isc);
+	vector<int> chosencols1;
+	for(int i = 0; i < cols; i++)
+		chosencols1.push_back(csc[i].id);
+	sort(chosencols1.begin(), chosencols1.end());
+	
+	vector<float> chosenfm1;
+	copy_subfreq(fm1, chosencols1, chosenfm1);
+	
+	vector<int>::iterator coliter = chosencols1.begin();
+	int offset = chosencols1[0];
+	for(; coliter != chosencols1.end(); ++coliter)
+		*coliter -= offset;
+	
+	double bestc = -1.1;
+	double c = 0.0;
+	for(int i = 0; i < fmsize2; i++) {
+		vector<int> chosencols2;
+		coliter = chosencols1.begin();
+		for(; coliter != chosencols1.end(); ++coliter)
+			chosencols2.push_back(i + *coliter);
+		vector<float> chosenfm2;
+		copy_subfreq(fm2, chosencols2, chosenfm2);
+		c = corr(chosenfm1, chosenfm2);
+		if(c > bestc)
+			bestc = c;
 	}
 	
-	// Score sites for the other motif with both PWMs
-	vector<Site>::const_iterator siter2 = m2.sites().begin();
-	for(; siter2 != m2.sites().end(); ++siter2) {
-		c = siter2->chrom();
-		p = siter2->posit();
-		s = siter2->strand();
-		len = seqset.len_seq(c);
-		
-		max_sc_1 = -1000;
-		window_size = w2 - olap;
-		for(int i = max(0, p - window_size); i < min(len - w1 - 1, p + window_size); i++) {
-			sc = m1.score_site(sm1, c, i, s) - bgmodel.score_site(m1.first_column(), m1.last_column(), w1, c, i, s);
-			sc /= m1.ncols();
-			if(sc > max_sc_1)
-				max_sc_1 = sc;
-		}
-		scores1.push_back(max_sc_1);
-		
-		max_sc_2 = -1000;
-		window_size = w1 - olap;
-		for(int i = max(0, p - window_size); i < min(len - w2 - 1, p + window_size); i++) {
-			sc = m2.score_site(sm2, c, i, s) - bgmodel.score_site(m2.first_column(), m2.last_column(), w2, c, i, s);
-			sc /= m2.ncols();
-			if(sc > max_sc_2)
-				max_sc_2 = sc;
-		}
-		scores2.push_back(max_sc_2);
-	}
-	delete [] sm1;
-	delete [] sm2;
-	
-	assert(scores1.size() == (unsigned int) m1.number() + m2.number());
-	assert(scores2.size() == (unsigned int) m1.number() + m2.number());
-	
-	float mcorr = corr(scores1, scores2);
-	return mcorr;
+	return bestc;
 }
 
+void MotifCompare::copy_subfreq(const vector<float>& fm, const vector<int>& cols, vector<float>& subfm) const {
+	vector<int>::const_iterator col_iter = cols.begin();
+	for(; col_iter != cols.end(); ++col_iter)
+		for(int j = 0; j < 4; j++)
+			subfm.push_back(fm[*col_iter * 4 + j]);
+}
--- src/motifcompare.h
+++ src/motifcompare.h
@@ -7,6 +7,17 @@ class MotifCompare {
 	const Seqset& seqset;
 	const BGModel& bgmodel;
 	
+	struct idscore {
+		int id;
+		float score;
+	};
+	
+	struct iscomp {
+		bool operator() (struct idscore is1, struct idscore is2) { return (is1.score < is2.score); }
+	} isc;
+	
+	void copy_subfreq(const vector<float>& fm, const vector<int>& cols, vector<float>& subfm) const;
+	
 public:
 	MotifCompare(const Seqset& s, const BGModel& bgm);
 	float compare(const Motif& m1, const Motif& m2) const;
