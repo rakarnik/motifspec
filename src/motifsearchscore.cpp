15a9f086deb4d8839198314460d95216eb3fbc59
Rahul Karnik <rahul@genebrew.com>
2012-02-08 01:55:47 -0500

    Turn off learning of number of columns

--- src/motifsearchscore.cpp
+++ src/motifsearchscore.cpp
@@ -104,10 +104,9 @@ int MotifSearchScore::search_for_motif(const int worker, const int iter, const s
 			single_pass(false);
 		else
 			single_pass_select(false);
-		if(i % 5 == 0 && motif.ncols() < 15)
-			motif.column_sample(true, false);
-		if((i + 3) % 5 == 0 && motif.ncols() > 6)
-			motif.column_sample(false, true);
+		for(int j = 0; j < 3; j++) {
+			if(! motif.column_sample()) break;
+		}
 		compute_seq_scores_minimal();
 		update_seq_count();
 		motif.set_motif_score(score());
