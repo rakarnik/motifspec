d84d30af211dc71b71c75df9ac2f207eb1315dcf
Rahul Karnik <rahul@genebrew.com>
2012-04-18 02:17:49 -0400

    Output archive if we have added 20 motifs, otherwise archive thread cannot keep up with worker thread output

--- src/inspector.cpp
+++ src/inspector.cpp
@@ -228,7 +228,7 @@ int read_motifs(MotifSearch* ms) {
 	unsigned long pos = 0;
 	workdir = opendir(".");
 
-	while((dirp = readdir(workdir))) {
+	while(nmot <= 20 && (dirp = readdir(workdir))) {
 		filename = string(dirp->d_name);
 		len = filename.length();
 		pos = filename.find_last_of('.');
